5e77a0f5c67d6526582b3c57504df388848c624b
cmullaney12 <cmullaney12@cox.net>
2019-03-26 12:07:06 -0400

    Update genstations script to read from station_locs and add lat/lon to stations

--- api/core/management/commands/genstations.py
+++ api/core/management/commands/genstations.py
@@ -8,15 +8,19 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--csvs", "-c", nargs="+", required=True)
+        parser.add_argument("--locs", "-l", default="data/station_locs.json")
         parser.add_argument("--output", "-o", default="data/stations.json")
 
-    def handle(self, *args, csvs, output, **options):
+    def handle(self, *args, csvs, locs, output, **options):
         rows = []
         for csv_file in csvs:
             with open(csv_file) as f:
                 lines = f.read().splitlines()
                 reader = csv.DictReader(lines)
                 rows += reader
+
+        with open(locs) as f:
+            station_locs = json.load(f)
 
         stations = {}
         for row in rows:
@@ -26,6 +30,8 @@ class Command(BaseCommand):
                     "gtfs_id": station_id,
                     "name": row["STATION_NAME"],
                     "lines": [],
+                    "lat": station_locs.get(station_id,{}).get("lat")
+                    "lon": station_locs.get(station_id,{}).get("lon")
                 }
 
         with open(output, "w") as f:
