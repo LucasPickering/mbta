04a3707324f7a832df08c6b2c8e8faa1c4a5e66f
Lucas Pickering <lucas@lucaspickering.me>
2019-04-01 12:46:45 -0400

    Improve marks for stations

--- webapp/src/components/MapStation.tsx
+++ webapp/src/components/MapStation.tsx
@@ -1,4 +1,6 @@
 import Typography from '@material-ui/core/Typography';
+import Leaflet from 'leaflet';
+import { mapValues } from 'lodash-es';
 import React from 'react';
 import { Circle, Marker } from 'react-leaflet';
 
@@ -6,8 +8,32 @@ import { Station } from '../types';
 import { formatLines } from '../util';
 import Popup from './leaflet/Popup';
 
-const SIZE_FACTOR: number = 10;
-const CIRCLE_OPACITY = 0.25;
+const SIZE_FACTOR: number = 50;
+
+const LINE_ICONS = mapValues(
+  {
+    blue:
+      'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+    green:
+      'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+    orange:
+      'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-orange.png',
+    red:
+      'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+    silver:
+      'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
+  },
+  url =>
+    Leaflet.icon({
+      iconUrl: url,
+      shadowUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+      iconSize: [13, 21],
+      iconAnchor: [6, 21],
+      popupAnchor: [1, -20],
+      shadowSize: [21, 21],
+    })
+);
 
 interface Props {
   station: Station;
@@ -19,28 +45,25 @@ const MapStation: React.ComponentType<Props> = ({
   entries,
 }) => {
   const position: [number, number] = [lat, lon];
-  const circleRadius = (entries || 0) * SIZE_FACTOR;
-  const circleOpacity = CIRCLE_OPACITY / lines.length;
+  // sqrt the entries so that we scale the area instead of the radius
+  const circleRadius = Math.sqrt(entries || 0) * SIZE_FACTOR;
 
   return (
     <>
-      <Marker position={position}>
+      <Marker position={position} icon={LINE_ICONS[lines[0]]}>
         <Popup position={position}>
           <Typography variant="h6">{name}</Typography>
           <Typography variant="subtitle2">{formatLines(lines)}</Typography>
           <Typography>{entries} entries</Typography>
         </Popup>
       </Marker>
-      {lines.map(line => (
-        <Circle
-          key={line}
-          center={position}
-          radius={circleRadius}
-          stroke={false}
-          color={line}
-          fillOpacity={circleOpacity}
-        />
-      ))}
+
+      <Circle
+        center={position}
+        radius={circleRadius}
+        stroke={false}
+        color="black"
+      />
     </>
   );
 };
