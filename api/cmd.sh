85eb650c14f08b904d37e26becbe2bdbb0a57419
Lucas Pickering <lucas@lucaspickering.me>
2019-08-21 17:36:49 -0400

    Take secrets from files

--- api/cmd.sh
+++ api/cmd.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo "Reading DB password from $MBTA_DB_PASSWORD_FILE"
+export MBTA_DB_PASSWORD=$(cat $MBTA_DB_PASSWORD_FILE)
+echo "Reading secret key from $MBTA_SECRET_KEY_FILE"
+export MBTA_SECRET_KEY=$(cat $MBTA_SECRET_KEY_FILE)
 dockerize -wait tcp://${MBTA_DB_HOST}:5432
 ./manage.py migrate
 gunicorn mbta.wsgi -b :8000 \
